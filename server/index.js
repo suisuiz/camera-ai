b78846331f893c1c0b9bb7120954376e0a4f5ea9
suisuiz <wangyumin1996@gmail.com>
2023-10-16 11:54:28 +0800

    feat: add server.js update html

---
+++ server/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+'get handleDeviceByName': ctx => ctx,
+'get controlDevice': ctx => ctx,
+"post /stream/add": (ctx) => ctx,
+"post /stream/receiver": (ctx) => ctx,
+"get /stream/codec": (ctx) => ctx,
+};
