96d8819a5321c6cabffd342debf545c8dbbf24bc
suisuiz <wangyumin1996@gmail.com>
2023-10-25 08:59:32 +0800

    feat: update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,54 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-
-Vue.use(VueRouter);
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: '/',
+    name: 'Guide',
+    component: () => import(/* webpackChunkName: "guide" */ '@/views/guide/Guide.vue')
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    // path: '/device/:from',
+    path: '/device',
+    name: 'DeviceList',
+    component: () => import(/* webpackChunkName: "guide" */ '@/views/guide/DeviceList.vue')
   },
-];
+  {
+    // path: '/scene/:name/:from',
+    path: '/scene',
+    name: 'Scene',
+    component: () => import(/* webpackChunkName: "scene" */ '@/views/scenes/Scene.vue')
+  },
+  {
+    path: '/screenFull',
+    name: 'ScreenFull',
+    component: () => import(/* webpackChunkName: "scene" */ '@/views/scenes/ScreenFull.vue')
+  },
+  {
+    path: '/file',
+    name: 'FileInfo',
+    component: () => import(/* webpackChunkName: "file" */ '@/views/scenes/FileInfo.vue')
+  },
+  {
+    path: '/dateil',
+    name: 'FileDetail',
+    component: () => import(/* webpackChunkName: "file" */ '@/views/scenes/FileDetail.vue')
+  }
+]
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes,
-});
+  // mode: 'history',
+  routes
+})
 
-export default router;
+router.beforeEach((to, from, next) => {
+  if (to.name === 'Guide' || to.name === 'DeviceList') return next()
+  if (from.name === 'DeviceList') return next()
+
+  const cameraList = window.sessionStorage.getItem('cameraList')
+  if (!cameraList) return next('/')
+  next()
+})
+
+export default router
