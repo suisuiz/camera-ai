d64bb051d967b7df7132b06066d8f9ce7b0c5e0a
suisuiz <wangyumin1996@gmail.com>
2023-11-01 19:27:40 +0800

    feat: add vant

---
+++ src/plugins/vant.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+
+// 按需引入 vant 组件
+import { Overlay, Loading, Slider, Switch, Swipe, SwipeItem, Lazyload, Image as VanImage, Dialog, Toast, lazyload } from 'vant'
+
+Vue.use(Overlay)
+Vue.use(Loading)
+Vue.use(Slider)
+Vue.use(Switch)
+Vue.use(Swipe)
+Vue.use(SwipeItem)
+Vue.use(Lazyload)
+Vue.use(VanImage)
+Vue.use(Dialog)
+Vue.use(Toast)
+Vue.use(lazyload)
