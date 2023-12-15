d4f8c000f90ef4af64389fa70e4e2e911bb54d8b
suisuiz <wangyumin1996@gmail.com>
2023-12-15 17:48:03 +0800

    feat: update file

--- src/main.js
+++ src/main.js
@@ -1,12 +1,38 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
 
-Vue.config.productionTip = false;
+import axios from 'axios'
+
+import global from './global/global'
+Vue.prototype.GLOBAL = global
+
+// 引入全局 js
+import '@/plugins/vant'
+import '@/assets/js/flexable.js'
+import '@/assets/js/adapter-latest.js'
+import '@/assets/js/js-sdk.umd.js'
+Vue.config.productionTip = false
+
+// 添加响应拦截器 headers 里加 token
+axios.defaults.timeout = 30000
+axios.interceptors.request.use(
+  (config) => {
+    let token = window.sessionStorage.getItem('token')
+    if (token) config.headers.token = token
+    config.headers.common = {
+      Accept: 'application/json, text/plain, */*'
+    }
+    // console.log(config)
+    return config
+  },
+  (err) => {
+    console.log('error' + err)
+    return Promise.reject(err)
+  }
+)
 
 new Vue({
   router,
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: (h) => h(App)
+}).$mount('#app')
